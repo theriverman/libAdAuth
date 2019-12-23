15663e188cefa9974a71f354953d12ea4f8afda0
Kristof <kristof@daja.hu>
2019-12-23 16:00:56 +0100

    Config corrections in VS project and solution

---
+++ windowsMain.cpp
@@ -0,0 +1,24 @@
+/*
+	DEVELOPMENT-ONLY FILE
+*/
+#include <iostream>
+#include "libAdWindows.h"
+
+int main()
+{
+	// Credentials provided directly in source file during development
+	const char *dn = "************";
+	const char *pw = "************";
+	const char *hst = "***********";
+	const char *dmn = "***********";
+
+	// Cast const char* -> char*
+	auto username = (char*)dn;
+	auto password = (char*)pw;
+	auto host = (char*)hst;
+	auto domain = (char*)dmn;
+
+	int res = auth(username, password, host, domain, 0);
+
+	return res;
+}
