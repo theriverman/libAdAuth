df06b65266894b1347452bdf4c4d57546e61aeec
Kristof Daja <kristof@daja.hu>
2019-12-23 15:10:34 +0100

    Windows implementation added

---
+++ adAdapterWindows.cpp
@@ -0,0 +1,82 @@
+#include "libAdWindows.h"
+
+#include <iostream>
+#include <Windows.h>
+#include <winldap.h>
+#include <stdlib.h>
+#include <string>
+#include <sstream>
+
+#include <memory>
+#include <stdexcept>
+
+wchar_t *convertCharArrayToLPCWSTR(char* charArray)
+{
+	wchar_t* wString = new wchar_t[4096];
+	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
+	return wString;
+}
+
+int authWindows(
+	char *username,
+	char *password,
+	char *host,
+	char *domain,
+	int *port
+) {
+
+	// Security Guards
+	if (!username || !password || !host || !domain) {
+		std::cerr << "Missing value: username, password, host or domain" << std::endl;
+	}
+	if (!port) {
+		port = new int;		// allocate memory, because port points to NULL
+		*port = LDAP_PORT;	// assign default value
+	}
+
+	// Initialise & Open LDAP Connection
+	LDAP* ld = ldap_init(convertCharArrayToLPCWSTR(host), *port);  // ld is the LDAP connection
+	LDAP_TIMEVAL connTimeout = { 5, 0 }; // 5sec 0ms timeout for LDAP connection
+	ULONG conn = ldap_connect(ld, &connTimeout);
+	if (conn == LDAP_SUCCESS) {
+		#ifdef DEBUG
+				std::cout << "Successful connection to LDAP server..." << std::endl;
+		#endif // DEBUG
+	}
+	else {
+		char hexString[20];
+		std::cerr << "Failed to connect to LDAP. Error: " << conn << " Hex: " << hexString << std::endl;
+		return conn;
+	}
+
+	// Interpolate username + domain into a DN username
+	std::string _DN = username + std::string("@") + domain;
+	char* DN = _strdup(_DN.c_str());
+
+	// Bind As User
+	ULONG bind = ldap_simple_bind_s(
+		ld,
+		convertCharArrayToLPCWSTR(DN), // <username>@<domain>
+		convertCharArrayToLPCWSTR(password) // Password
+	);
+
+	switch (bind)
+	{
+	case LDAP_SUCCESS:
+		#ifdef DEBUG
+				std::cout << "Successful BIND to LDAP server..." << std::endl;
+		#endif // DEBUG
+		break;
+	case LDAP_INVALID_CREDENTIALS:
+		std::cout << "Invalid Credentials..." << std::endl;
+		break;
+	default:
+		char hexString[20];
+		_itoa_s(bind, hexString, 20);
+		std::cerr << "Failed to BIND to LDAP. Error: " << bind << " Hex: " << hexString << std::endl;
+		break;
+	}
+	ldap_unbind(ld);  //terminate the LDAP session 
+
+	return (int)bind;
+}
---
+++ dllmain.cpp
@@ -0,0 +1,39 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+
+#ifdef _WIN32
+
+#include "stdafx.h"
+#include <iostream>
+#include "libAdWindows.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD  ul_reason_for_call,
+                      LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+		break;
+    case DLL_THREAD_ATTACH:
+		break;
+    case DLL_THREAD_DETACH:
+		break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+#ifdef DEBUG
+DLLEXPORT int foo() {
+	std::cout << "Hello from foo()" << std::endl;
+	return 0;
+}
+#endif // DEBUG
+
+
+DLLEXPORT int auth(char *username, char *password, char *host, char *domain, int *port) {
+	return authWindows(username, password, host, domain, port);
+}
+#endif
