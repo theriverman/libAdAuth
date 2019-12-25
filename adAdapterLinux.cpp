606f6cc0885c31c54d3e598609abe9ecda2f00b4
Kristof Daja <kristof@daja.hu>
2019-12-25 04:16:42 +0100

    (Migration + Improvement)
      * Moved from `ldap_simple_bind_s` to `ldap_sasl_bind_s`.
      * Added `ldap_unbind_ext_s` call.

--- adAdapterLinux.cpp
+++ adAdapterLinux.cpp
@@ -7,7 +7,6 @@
 #include <string>
 #include <stdlib.h>
 
-#define LDAP_DEPRECATED 1 // Enable deprecated
 #include <ldap.h>
 
 int authLinux(
@@ -18,46 +17,58 @@ int authLinux(
 	int  *port
 ) {
 	#ifdef DEBUG
-		std::cout << "Auth running in adAdapterLinux.cpp" << std::endl;
-		std::cout << "DEBUG>> On Linux, only simple is suppored over port 389" << std::endl;
-		std::cout << "DEBUG>> Provided port number (if any) has been disregarded and using default (=389)" << std::endl;
+	std::cout << "(DEBUG) Provided port number (if any) has been disregarded and using default (=389)" << std::endl;
 	#endif
 	
+	// Declarations
+	int conn_status, version(LDAP_VERSION3), bind, unbind;
+	char *server;
+
 	// LDAP Handler
 	LDAP *ld;
 	
 	// Handler initialisation
-	std::string _server = std::string("ldap://") + host;  // <username>@<domain>
-	char* SERVER = strdup(_server.c_str());  // cast std::string -> char*
-
-	int status = ldap_initialize(&ld, SERVER);
-	if (status != LDAP_SUCCESS) {
-		std::cerr << "Error Code = " << status << " Message = " << ldap_err2string(status) << std::endl;
+	server = strdup(std::string("ldap://").append(host).c_str()); // Adds `ldap://` prefix to host
+	conn_status = ldap_initialize(&ld, server);
+	if (conn_status != LDAP_SUCCESS) {
+		std::cerr << "Error Code = " << conn_status << " Message = " << ldap_err2string(conn_status) << std::endl;
 	}
-	int version(LDAP_VERSION3);
 	ldap_set_option(ld, LDAP_OPT_PROTOCOL_VERSION, &version);
 
-	// Interpolate username + domain into a DN username
-	std::string _DN = username + std::string("@") + domain;  // <username>@<domain>
-	char* DN = strdup(_DN.c_str());
+	// Append domain to username
+	username = strdup((std::string(username).append("@").append(domain)).c_str());  // <sAMAccountName>@<domain>
 
 	// Execute bind
-	// int res = ldap_sasl_bind_s(ld, DN, LDAP_SASL_AUTOMATIC, &credentials, NULL, NULL, NULL);  // SASL [NOK]
-	// int res = ldap_gssapi_bind_s(ldapHandler, DN, pw); // GSSAPI [NOK]
-	/*
-		LDAP_F( int )
-		ldap_simple_bind_s LDAP_P((
-			LDAP *ld,
-			LDAP_CONST char *who,
-			LDAP_CONST char *passwd ));
-	*/
-	int res = ldap_simple_bind_s(ld, DN, password);  // [deprecated - OK]
-	if(res != LDAP_SUCCESS) {
-		std::cerr << "Bind failed. Error Code: " << res << " | Message: " << ldap_err2string(res) << std::endl;
+	berval credentials;
+	berval *srv = NULL;
+	credentials.bv_val = password;
+	credentials.bv_len = strlen(password);
+
+	bind = ldap_sasl_bind_s(
+        ld,
+        username, // DN ==>    <sAMAccountName>@<domain>
+        LDAP_SASL_SIMPLE,
+        &credentials,
+        NULL,
+        NULL,
+        &srv
+	);
+
+	if (bind != LDAP_SUCCESS) {
+		std::cerr << "Bind failed. Error Code: " << bind << " | Message: " << ldap_err2string(bind) << std::endl;
+		return bind;
 	} else {
 		std::cout << "Bind success !" << std::endl;
 	}
-    return res;
+
+	// Unbind
+	unbind = ldap_unbind_ext_s(ld, NULL, NULL);
+	if (unbind != LDAP_SUCCESS) {
+		std::cerr << "Unbind failed. Error Code: " << unbind << " | Message: " << ldap_err2string(bind) << std::endl;
+	} else {
+		std::cout << "Unbind success !" << std::endl;
+	}
+    return bind;
 }
 
 #endif
