fc2169d644d02736f7de91cf533e854135c547bd
Kristof Daja <kristof@daja.hu>
2019-12-25 04:18:28 +0100

    added new (commented) options to  the build script

--- buildForLinux.sh
+++ buildForLinux.sh
@@ -6,11 +6,20 @@ export BASE_FILES_BIN="linuxMain.cpp adAdapterLinux.cpp libAdLinux.h"
 export BASE_FILES="sharedObject.cpp adAdapterLinux.cpp libAdLinux.h"
 export LIBLDAP="/usr/lib/x86_64-linux-gnu/libldap-2.4.so.2"
 export OUT_BIN="test_libAdAuthBin"
+export OUT_BIN_DEBUG="libAdAuthBinDebug"
 export OUT_SO="libAdAuth.so"
+export OUT_SO_DEBUG="libAdAuthDebug.so"
 
-# Build Test Binary
+# Build Test Binary [Debug]
+# g++ -std=c++17 -I. -DDEBUG $BASE_FILES_BIN $LIBLDAP -o $OUT_BIN_DEBUG
+
+# Build Test Binary [Release]
 # g++ -std=c++17 -I. $BASE_FILES_BIN $LIBLDAP -o $OUT_BIN
 
-# Build Shared Object Library
+# Build Shared Object Library [Debug]
+# g++ -std=c++17 -fPIC -c -Wall -DDEBUG $BASE_FILES
+# g++ -shared adAdapterLinux.o sharedObject.o $LIBLDAP -o $OUT_SO_DEBUG
+
+# Build Shared Object Library [Release]
 g++ -std=c++17 -fPIC -c -Wall $BASE_FILES
 g++ -shared adAdapterLinux.o sharedObject.o $LIBLDAP -o $OUT_SO
